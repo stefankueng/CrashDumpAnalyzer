ab0fa88162be139dde12f153ef248f576555ce91
Küng Stefan <Stefan.Kueng@zund.com>
2025-01-09 14:17:52 +0100

    show link to project and version/hash in footer

---
+++ CrashDumpAnalyzer/Utilities/VersionHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace CrashDumpAnalyzer.Utilities
+{
+    public class VersionHelper
+    {
+        public static string GetAssemblyVersion()
+        {
+            var infoVersion = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false).FirstOrDefault() as AssemblyInformationalVersionAttribute;
+            return infoVersion.InformationalVersion;
+        }
+    }
+}
