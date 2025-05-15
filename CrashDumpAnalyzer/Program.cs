2a04bada9b80e2e7d17eefbcdee591033f521508
Stefan Küng <stefankueng@gmail.com>
2025-05-15 14:20:17 +0200

    use a default connection string

--- CrashDumpAnalyzer/Program.cs
+++ CrashDumpAnalyzer/Program.cs
@@ -16,7 +16,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "./webapplication.db";
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
